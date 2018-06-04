7091c725a9bb63dfd01f0b4b742b740b984aa829
Calvin Yan <calvin.yan@sony.com>
2018-06-03 17:50:32 -0700

    Remove keyboard shortcut to open search manager

--- public/background.js
+++ public/background.js
@@ -7,10 +7,3 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
   openNewSearchManager();
 });
 
-chrome.commands.onCommand.addListener(function(command) {
-  // Users can bind a key to this command in their Chrome
-  // keyboard shortcuts, at the bottom of their extensions page.
-  if (command == 'open-search-manager') {
-    openNewSearchManager();
-  }
-});
